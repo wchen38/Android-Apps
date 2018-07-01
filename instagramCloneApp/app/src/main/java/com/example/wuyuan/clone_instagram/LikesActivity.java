6d4cdb170bd7d715d8c366d834b1b7f2a2fe04a0
Wuyuan <wuyuan@pureengineering.com>
2018-07-01 15:52:13 -0700

    bottom navigation can go to different activities

--- instagramCloneApp/app/src/main/java/com/example/wuyuan/clone_instagram/LikesActivity.java
+++ instagramCloneApp/app/src/main/java/com/example/wuyuan/clone_instagram/LikesActivity.java
@@ -1,8 +1,9 @@
 package com.example.wuyuan.clone_instagram;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,18 +11,17 @@ import android.view.MenuItem;
 import com.example.wuyuan.clone_instagram.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class HomeActivity extends AppCompatActivity {
+public class LikesActivity extends AppCompatActivity{
+    private static final String TAG = "LikesActivity";
+    private static final int ACTIVITY_NUM = 3;
 
-    private static final String TAG = "HomeActivity";
-    private static final int ACTIVITY_NUM = 0;
+    private Context mContext = LikesActivity.this;
 
-    private Context mContext = HomeActivity.this;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Log.d(TAG, "onCreate: starting");
-
+        Log.d(TAG, "onCreate: started.");
         setupBottomNavigationView();
     }
 
