503dedb3c89c8ef9da4a59e9cd736c0480ef8349
Wuyuan <wuyuan@pureengineering.com>
2018-07-02 16:19:26 -0700

    added profile activity layout, video 9

--- instagramCloneApp/app/src/main/java/com/example/wuyuan/clone_instagram/Profile/ProfileActivity.java
+++ instagramCloneApp/app/src/main/java/com/example/wuyuan/clone_instagram/Profile/ProfileActivity.java
@@ -24,7 +24,7 @@ public class ProfileActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
-        //setupBottomNavigationView();
+        setupBottomNavigationView();
         setUpToolbar();
     }
 
