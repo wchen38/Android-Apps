dcbbd2bfca908894eec50d68e8acdf0e4dbbcd09
Wuyuan <wuyuan@pureengineering.com>
2018-07-01 17:48:15 -0700

    menu working at the top of the profile, video tutoiral part 7

--- instagramCloneApp/app/src/main/java/com/example/wuyuan/clone_instagram/Profile/ProfileActivity.java
+++ instagramCloneApp/app/src/main/java/com/example/wuyuan/clone_instagram/Profile/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import com.example.wuyuan.clone_instagram.R;
 import com.example.wuyuan.clone_instagram.Utils.BottomNavigationViewHelper;
@@ -21,9 +22,30 @@ public class ProfileActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+        setUpToolbar();
+    }
+
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch (item.getItemId()) {
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigating to profile Preferences");
+                        break;
+
+                }
+                return false;
+            }
+        });
     }
 
     /**
@@ -37,5 +59,11 @@ public class ProfileActivity extends AppCompatActivity{
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
