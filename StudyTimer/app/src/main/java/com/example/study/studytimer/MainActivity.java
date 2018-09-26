b8b59a1a786a5d9cc6b18da2bf52fce10df31934
Wuyuan <wuyuan@pureengineering.com>
2018-09-25 21:44:55 -0700

    study timer app

---
+++ StudyTimer/app/src/main/java/com/example/study/studytimer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.study.studytimer;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+}
